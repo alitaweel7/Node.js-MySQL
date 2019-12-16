73514f3f82cd879cf3dd6980560fac3e11b52ae8
Ali Mustafa <ali.taweel7@gmail.com>
2019-12-15 21:05:05 -0500

    database connected, and Json file added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,30 @@
+// VARIABLES 
+
+// Adding dependencies and storing into variables
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+
+// var server = http.createServer(handleRequest);
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Taw9951019797',
+    database: 'Bamazon'
+});
+
+
+
+
+// FUNCTIONS //
+
+
+
+// Connects to database and console logs connection
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected as id " + connection.threadId);
+    start();
+})
 
