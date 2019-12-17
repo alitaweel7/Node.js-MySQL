7b6b06117855f2fc1f50e288f57e399c9d2a8606
Ali Mustafa <ali.taweel7@gmail.com>
2019-12-16 19:37:40 -0500

    fixed inquirer issue

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,4 @@
 // VARIABLES 
-
 // Adding dependencies and storing into variables
 var mysql = require('mysql');
 var inquirer = require('inquirer');
@@ -13,34 +12,28 @@ var connection = mysql.createConnection({
     password: 'Taw9951019797',
     database: 'Bamazon'
 });
-
-
-
-
 // FUNCTIONS //
-
-
-
 // Connects to database and console logs connection
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
-    start();
-})
+    start()
+});
 
 
 // Starts Bamazon 
 var start = function () {
     connection.query('SELECT * FROM products', function (err, res) {
         for (var i = 0; i < res.length; i++) {
-
             console.log('Item Id: ' + res[i].item_id + ' '
-                + ' Product: ' + res[i].product_name + ' ' + "DEPARTMENT: " + res[i].department_name + ' Price: $' + res[i].price + ' | ' + ' Available: ' + res[i].stock_quantity);
+                + ' Product: ' + res[i].product_name + ' ' + "DEPARTMENT: " + res[i].department_name + ' Price: $' + res[i].price + ' | ' + ' Available: ' + res[i].stock);
         }
         purchaseItem();
     })
 
 }
+
+
 
 // Function to purchase an item.
 var purchaseItem = function () {
@@ -70,10 +63,8 @@ var purchaseItem = function () {
 
     }]).then(function (answer) {
         connection.query('SELECT * FROM products WHERE item_id = ?', [answer.item_id], function (err, res) {
-            console.log(res);
             if (answer.stock_quantity > res[0].stock_quantity) {
                 console.log('INSUFFICIENT QUANTITY');
-
                 quitBamazon();
             } else {
                 let priceTotal = res[0].price * answer.stock_quantity;
@@ -81,9 +72,10 @@ var purchaseItem = function () {
                 console.log('Your Total Amount is $' + priceTotal);
                 console.log('Thank you for your order');
                 console.log('');
-                var math = res[0].stock_quantity - parseInt(answer.stock_quantity);
-                connection.query('UPDATE products SET stock_quantity= ' + math + ' WHERE item_id =' + answer.item_id);
+                var math = res[0].stock - parseInt(answer.stock_quantity);
+                connection.query(`UPDATE products SET stock=${math} WHERE item_id=${answer.stock_quantity};`);
                 quitBamazon();
+
             }
         });
     });
@@ -92,6 +84,7 @@ var purchaseItem = function () {
 
 
 // Function to quit Bamazon
-var quitBamazon = function () {
+const quitBamazon = function () {
     connection.end();
 }
+
